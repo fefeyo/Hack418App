094538abf16b9bcd0871504b7a098eaa2e0f86ed
sakatayu <imanity.fefe@gmail.com>
2015-10-03 14:42:39 +0900

    'project_start'

---
+++ wear/src/main/java/com/team_a/hack418app/MainScreen.java
@@ -0,0 +1,24 @@
+package com.team_a.hack418app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainScreen extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
